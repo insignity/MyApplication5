7dfd0297066fd1c8c8cca2f4badd94464679bfed
Айарсиэн Дьячковский <ayarsen1995@gmail.com>
2019-05-14 14:33:38 +0900

    fixed DBHelper

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -1,74 +1,87 @@
 package com.example.myapplication;
 
-import android.database.SQLException;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+public class DBHelper extends SQLiteOpenHelper {
+    public static int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "db35";
+    public static final String TABLE1 = "table1";
+    public static final String TABLE2 = "table2";
+    public static final String TABLE3 = "table3";
+    public static final String TABLE4 = "table4";
+    public static final String TABLE5 = "table5";
+    public static final String TABLE6 = "table6";
+    public static final String TABLE7 = "table7";
+    public static final String TABLE8 = "table8";
+    public static final String USER = "user";
 
-class DBHelper extends SQLiteOpenHelper {
-    private static String DB_PATH; // полный путь к базе данных
-    private static String DB_NAME = "cityinfo.db";
-    private static final int SCHEMA = 1; // версия базы данных
-    static final String TABLE = "users"; // название таблицы в бд
-    // названия столбцов
-    static final String COLUMN_ID = "_id";
-    static final String COLUMN_NAME = "name";
-    static final String COLUMN_YEAR = "year";
-    private Context myContext;
+    public static final String COL_ID = "id";
+    public static final String COL_NAME = "name";
+    public static final String COL_SRC = "imgsrc";
+    public static final String COL_PERCENTAGE = "percentage";
+    public final String TAG = "myLogs";
 
-    DBHelper(Context context) {
-        super(context, DB_NAME, null, SCHEMA);
-        this.myContext=context;
-        DB_PATH =context.getFilesDir().getPath() + DB_NAME;
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "Database was created");
+        db.execSQL("CREATE TABLE " + TABLE1 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE2 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE3 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE4 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE5 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE6 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE7 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE8 + "(" + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + COL_NAME + " TEXT," + COL_SRC + " TEXT)");
+        db.execSQL("CREATE TABLE " + USER + "(" + COL_ID + " INTEGER," + COL_PERCENTAGE + " INTEGER)");
+
     }
+
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion,  int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS "+USER + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE1 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE2 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE3 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE4 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE5 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE6 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE7 + ";");
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE8 + ";");
+        onCreate(db);
+        Log.d(TAG, "Database was updated");
     }
 
-    void create_db(){
-        InputStream myInput = null;
-        OutputStream myOutput = null;
-        try {
-            File file = new File(DB_PATH);
-            if (!file.exists()) {
-                this.getReadableDatabase();
-                //получаем локальную бд как поток
-                myInput = myContext.getAssets().open(DB_NAME);
-                // Путь к новой бд
-                String outFileName = DB_PATH;
-
-                // Открываем пустую бд
-                myOutput = new FileOutputStream(outFileName);
-
-                // побайтово копируем данные
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = myInput.read(buffer)) > 0) {
-                    myOutput.write(buffer, 0, length);
-                }
-
-                myOutput.flush();
-                myOutput.close();
-                myInput.close();
-            }
-        }
-        catch(IOException ex){
-            Log.d("DatabaseHelper", ex.getMessage());
+    public void insertData(int table, int id, String name, String imgsrc) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ID, id);
+        contentValues.put(COL_NAME, name);
+        contentValues.put(COL_SRC, imgsrc);
+        switch (table){
+            case 1: db.insert(TABLE1, null, contentValues); break;
+            case 2: db.insert(TABLE2, null, contentValues); break;
+            case 3: db.insert(TABLE3, null, contentValues); break;
+            case 4: db.insert(TABLE4, null, contentValues); break;
+            case 5: db.insert(TABLE5, null, contentValues); break;
+            case 6: db.insert(TABLE6, null, contentValues); break;
+            case 7: db.insert(TABLE7, null, contentValues); break;
+            case 8: db.insert(TABLE8, null, contentValues); break;
         }
     }
-    public SQLiteDatabase open()throws SQLException {
-
-        return SQLiteDatabase.openDatabase(DB_PATH, null, SQLiteDatabase.OPEN_READWRITE);
+    public void insertUserData(int id, int percent) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ID, id);
+        contentValues.put(COL_PERCENTAGE, percent);
+        db.insert(USER, null, contentValues);
     }
 }
